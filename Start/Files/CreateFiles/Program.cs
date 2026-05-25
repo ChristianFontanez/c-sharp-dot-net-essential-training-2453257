1ad2cdf843ba89ee4622280311bbf96c06c69882
XianCodes <christfontanez@gmail.com>
2026-05-25 12:40:07 -0400

    finished creating files and reading and writing files video

--- Start/Files/CreateFiles/Program.cs
+++ Start/Files/CreateFiles/Program.cs
@@ -5,6 +5,27 @@ const string filename = "TestFile.txt";
 
 // TODO: Create a new file - this will overwrite any existing file
 // Use the "using" construct to automatically close the file stream
+//using(StreamWriter sw = File.CreateText(filename))
+//{
+//    sw.WriteLine("This is a text file.");
+//}
+// Garbage collector cannot automatically deal with filesystem files. We need to use this using keyword 
 
 
 // TODO: Determine if a file exists
+Console.WriteLine(File.Exists(filename));
+if (File.Exists(filename))
+{
+    File.Delete(filename);
+}
+else
+{
+    using(StreamWriter sw = File.CreateText(filename))
+    {
+        sw.WriteLine("This is a text file.");
+    }
+}
+    
+Console.WriteLine(File.Exists(filename));
+
+
